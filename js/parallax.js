31a7e5b82913bd0e905fa26b4bf15b289c9388fd
Cameron Gale <cameroneldongale@gmail.com>
2016-09-01 09:35:46 -0600

    nailed it

---
+++ js/parallax.js
@@ -0,0 +1,19 @@
+$(window).scroll(function(){
+
+var winScroll = $(this).scrollTop()
+
+$('.left-branch').css({
+  'transform': 'translate(-'+ winScroll /10 +'%, 0px)'
+  })
+
+$('.right-branch').css({
+  'transform' : 'translate('+ winScroll /10 +'%, 0px)'
+})
+
+$('.vader').css({
+    'transform': 'translate(0px, '+ winScroll /10 +'%)'
+  });
+
+
+
+})
